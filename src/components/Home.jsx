5ca62fada54b1693f2be7fa3188cf8b5758ab970
LuisCruzGareca <garecal447@gmail.com>
2025-05-20 15:06:47 -0400

    fixed imports at home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import "../App.css";
 import "../css/Movile.css";
 
 import { Footer } from "./Footer";
-import { Header } from "./header";
-import { Main } from "./main";
+import { Header } from "./Header";
+import { Main } from "./Main";
 
 export function Home() {
   return (
