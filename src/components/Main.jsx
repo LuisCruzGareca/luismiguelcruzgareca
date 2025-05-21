78a8f7c18ec2080581ea45f2eb9bac977edcb8a4
LuisCruzGareca <garecal447@gmail.com>
2025-05-21 11:07:34 -0400

    restored main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import Portada from "./Portada";
+import Proyects from "./Proyects";
+import Tools from "./Tools";
+
+export default function Main() {
+  return (
+    <main id="SobreMi">
+      <Portada />
+      <Tools />
+      <Proyects />
+    </main>
+  );
+}
